134d453ff6ae9e8da2405c37466be175c522a307
Guilherme Aipp <marques.guilherme@protonmail.com>
2025-04-19 20:06:58 -0300

    Configura integração com Cypress Cloud via GitHub Actions V12

--- cypress/e2e/TC002 - Verifica título da aplicação.cy.js
+++ cypress/e2e/TC002 - Verifica título da aplicação.cy.js
@@ -4,7 +4,7 @@ describe("TC002 - Verifica título da aplicação", function() {
     });
   
     it("Verifica se o título da página é 'Automation Exercise'", function() {
-      cy.title().should('be.equal', 'utomation Exercise');
+      cy.title().should('be.equal', 'Automation Exercise');
     });
   });
   
